29b2f9e55ed0fb81f8e102b5708f1300268d37d1
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-08-09 23:39:17 -0700

    updated readme

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 # 06 Server-Side APIs: Weather Dashboard
 
-Coded HTML, CSS, and JavaScript from scratch. App is able to pull weather data from the Open Weather API. 
+Coded weather forecast app with HTML, CSS, and JavaScript from scratch. App is able to pull weather data from the Open Weather API. When weather is displayed there is a container for current weather, as well as dynamically created day cards for the five day weather forecast. 
 
 ## Areas addressed:
 
-* Individual day weather areas were created with corresponding time stamps and forecast elements.
-
+* Weather data fetched from Open Weather API. 
+* location data converted from lat and lon to city name.
+* Current weather data printed to current weather container and uv background color changes based on uv index number.
+* For loop created to pull weather data for the five da forecast. 
+* Five day weather data appended to day cards.
 
 
 ## Deployed application: (https://kentshaffer.github.io/weather-dashboard/)
 
+![image screenshot of current weather container and five day forecast on deployed application](./assets/images/weather%20dashboard%20screenshot.png) 
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,9 @@ let UV = document.querySelector('#uv');
 citySearchFormEl.addEventListener('submit', function(event){
   event.preventDefault()
   console.log(citySearchEL.value);
-  // var tempArray = JSON.parse(localStorage.getItem('cities')) || [] 
-  // tempArray.push()
+  var tempArray = JSON.parse(localStorage.getItem('cities')) || [] 
+  tempArray.push(citySearchEL.value)
+  localStorage.setItem('cities', JSON.stringify(tempArray))
   fetch('http://api.openweathermap.org/geo/1.0/direct?q='+citySearchEL.value+'&limit=1&appid='+apiKey).then(function(response){
     return response.json()
   }).then(function(data){
@@ -32,16 +33,11 @@ function searchWeather(lat, lon, cityName){
     return response.json()
   }).then(function(data){
     console.log(data)
-    // var currentCard = $('<div>').addClass('card');
-    // var cityNameEl = $('<h3>').addClass('card-title').text(cityName);
+
     var currentIcon = (data.current.weather[0].icon);
     var currentWeather = [data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi];
     var currentIconURL = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`;
-    // var currentIconEl = $('<h3>').addClass('card-title').text(currentIconURL);
-    // var currentWeatherEl = $('<ol>').addClass('current-weather').text(currentWeather);
-    // $('.current-city').append(currentCard.append(cityNameEl, currentIconEl, currentWeatherEl))
-    // $('.current-city-icon').append(currentCard.append(currentIconEl))
-    // $('#current-weather').append(currentCard.append(currentWeatherEl))
+
     currentWeatherTitleEl.innerText = cityName;
     currentWeatherIconEl.setAttribute('src', currentIconURL);
     let currentDate = new Date((data.current.dt)* 1000).toDateString();
@@ -52,12 +48,12 @@ function searchWeather(lat, lon, cityName){
     currentHumidityEl.innerText = 'Humidity:' + ' ' + currentWeather[2] + ' ' + '%';
     UV.innerText = 'UV Index' + ' ' + currentWeather[3];
 
-    if (currentWeather[3] > 0 && currentWeather[3] <= 3) {
-      UV.setAttribute('style', 'background-color: green; padding: 2px;');
+    if (currentWeather[3] >= 0 && currentWeather[3] <= 3) {
+      UV.setAttribute('style', 'background-color: springgreen; padding: 2px; width: fit-content;');
     } else if (currentWeather[3] > 3 && currentWeather[3] <= 5) {
-      UV.setAttribute('style', 'background-color: yellow; padding: 2px;');
+      UV.setAttribute('style', 'background-color: gold; padding: 2px; width: fit-content;');
     } else if (currentWeather[3] > 5) {
-      UV.setAttribute('style', 'background-color: red; padding: 2px;');
+      UV.setAttribute('style', 'background-color: tomato; padding: 2px; width: fit-content;');
     }
     console.log(currentWeather[1])
   })
@@ -106,17 +102,29 @@ var forecastDiv = document.querySelector('.five-day-forecast')
     forecastDiv.appendChild(cardDiv);
 
     }
-    var searchList = document.createElement('li');
-    var searchListButton = document.createElement('button');
-    searchListButton.setAttribute('class', 'prev-search');
-    searchListButton.textContent = document.querySelector('#search-input').value;
-    searchList.appendChild(searchListButton);
     
-    localStorage.setItem('searchedCities', document.querySelector('#search-input').value);
-    var prevSearch = document.getElementsByClassName('prev-search');
-    var prevSearchValue = prevSearch.textContent;
-    console.log(prevSearchValue);
+    // localStorage.setItem('searchedCities', document.querySelector('#search-input').value);
+    // var prevSearch = document.getElementsByClassName('.prev-search');
+    // var prevSearchValue = prevSearch.textContent;
+    // console.log(prevSearchValue);
   })
 }
 
-localStorage.getItem('searchedCities');
+
+function recallSearchHistory() {
+  var recallButton = document.querySelector('.prev-search');
+  for (var i = 0; i < JSON.parse(localStorage.getItem('cities')).length; i++) {
+    console.log(JSON.parse(localStorage.getItem('cities'))[i]);
+      var recallButtonEl = document.createElement('li');
+      recallButtonEl.setAttribute('class', 'recall-search');
+      recallButtonEl.textContent = 'cities';
+      recallButton.appendChild(recallButtonEl);
+    }
+  //   localStorage.getItem('searchedCities');
+
+  //   var searchListButton = document.createElement('button');
+  //   searchListButton.setAttribute('class', 'prev-search');
+  //   append(searchListButton);
+  }
+
+
